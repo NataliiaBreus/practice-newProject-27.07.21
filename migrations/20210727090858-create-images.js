ce90c30f794e7c4b9f81280ded8fc0757f1668a7
Nataliia Breus <breusnataly@gmail.com>
2021-07-27 12:16:41 +0300

    adde new migration images

---
+++ migrations/20210727090858-create-images.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      heroId: {
+        allowNull: false,
+        field: "hero_id",
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'superheroes',
+          key: 'id'
+
+        }
+      },
+      imagePath: {
+        field: "image_path",
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        field: "created_at",
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        field: "updated_at",
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('images');
+  }
+};
